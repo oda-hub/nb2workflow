361d998cd3377dccd3928a6d1d8003a4d8efb296
Volodymyr <vladimir.savchenko@gmail.com>
2018-11-02 16:08:41 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='nb2workflow',
+      version='1.0',
+      description='convert notebook to workflow',
+      author='Volodymyr Savchenko',
+      author_email='vladimir.savchenko@gmail.com',
+      license='MIT',
+      packages=['nb2workflow'],
+      zip_safe=False,
+     )
