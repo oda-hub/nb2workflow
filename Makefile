df75056b7bbd8a2b02551c965aa436e9eb0eadd1
Volodymyr <vladimir.savchenko@gmail.com>
2019-09-03 18:32:41 +0200

    slightly different version

--- Makefile
+++ Makefile
@@ -6,3 +6,9 @@ build: Dockerfile
 
 push: build
 	docker push $(image) 
+
+dist:
+	python setup.py sdist bdist_wheel
+
+upload: dist
+	twine upload --verbose --skip-existing -r oda dist/*
--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
+import sys
+
+setup_requires = ['setuptools >= 30.3.0', 'setuptools-git-version']
+if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
+    setup_requires.append('pytest-runner')
 
 setup(name='nb2workflow',
       version='1.0.1',
+      version_format = '{tag}.dev{commitcount}+{gitsha}',
       description='convert notebook to workflow',
       author='Volodymyr Savchenko',
       author_email='contact@volodymyrsavchenko.com',
@@ -38,5 +44,7 @@ setup(name='nb2workflow',
       download_url = 'https://github.com/volodymyrss/nb2workflow/archive/1.0.1.tar.gz',
       keywords = ['jupyter', 'docker'],
       classifiers = [],
-     )
+      setup_requires=setup_requires)
+
+
 
