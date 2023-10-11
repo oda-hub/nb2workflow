d830c1a752396365af95be1b7dc882b97bc9de8e
Volodymyr <volodymyrss@users.noreply.github.com>
2023-10-11 11:49:36 +0200

    Update setup.py (#114)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='nb2workflow',
       packages=['nb2workflow'],
       zip_safe=False,
 
-      python_requires='>=3.9',
+      python_requires='>=3.9,<3.12',
 
       entry_points={
           'console_scripts': [
