402ce9a087834eb7ce0b2cf5367c135b1388e9a5
burnout87 <gabriele.barni@gmail.com>
2024-04-17 11:23:06 +0200

    no need for conf_dir

--- nb2workflow/__init__.py
+++ nb2workflow/__init__.py
@@ -2,7 +2,6 @@ import pkg_resources
 import os
 
 name = "nb2workflow"
-conf_dir = os.path.dirname(__file__)+'/config_dir'
 
 def version():
     v = pkg_resources.get_distribution("nb2workflow").version
