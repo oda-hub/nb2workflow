9c7220c844508e8704a512e2579c06e0db08ef17
Gabriele Barni <burnout87@users.noreply.github.com>
2024-05-28 10:30:33 +0200

    bugfix updated url for the download, allow redirection for HEAD, more checks mmoda url, allow ip url validation (#178)
    * using the updated url for the download, allow redirection for HEAD method
    
    * adapted test
    
    * better logging
    
    * double routes for download endpoints
    
    * extended test
    
    * more robust mmoda url checked
    
    * adapted and extended tests
    
    * freeing validators version for allowing ip address validation

--- nb2workflow/url_helper.py
+++ nb2workflow/url_helper.py
@@ -3,6 +3,6 @@ from urllib.parse import urlparse
 
 def is_mmoda_url(url):
     parsed_url = urlparse(url)
-    if 'mmoda' in parsed_url.path:
+    if 'mmoda' in parsed_url.path or 'dispatch-data' in parsed_url.path or '_is_mmoda_url' in parsed_url.query:
         return True
     return False
