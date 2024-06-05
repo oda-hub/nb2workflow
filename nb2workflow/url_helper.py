5c89ee5c2774abfcc62ea54abef7ba869c2cdeb5
Denys Savchenko <56398430+dsavchenko@users.noreply.github.com>
2024-06-05 15:14:55 +0200

    serialise_workflow_exception in helpers (#184)

--- nb2workflow/url_helper.py
+++
@@ -1,8 +0,0 @@
-from urllib.parse import urlparse
-
-
-def is_mmoda_url(url):
-    parsed_url = urlparse(url)
-    if 'mmoda' in parsed_url.path or 'dispatch-data' in parsed_url.path or '_is_mmoda_url' in parsed_url.query:
-        return True
-    return False
