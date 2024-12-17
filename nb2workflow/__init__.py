08d61d3a64a26d8cd8f6b54380cca5855ac81510
Denys SAVCHENKO <savchenko@apc.in2p3.fr>
2024-12-17 10:54:09 +0100

    use importlib to get package version

--- nb2workflow/__init__.py
+++ nb2workflow/__init__.py
@@ -1,9 +1,9 @@
-import pkg_resources
+from importlib.metadata import version as pkg_ver
 
 name = "nb2workflow"
 
 def version(print_it=True):
-    v = pkg_resources.get_distribution("nb2workflow").version
+    v = pkg_ver("nb2workflow")
     if print_it:
         print(v)
 
